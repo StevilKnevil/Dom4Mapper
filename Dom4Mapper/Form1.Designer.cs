2079dbeec8156be3cb13c8445b34f9a2d2e4fb54
Stevil Knevil <stevilknevil@hotmail.co.uk>
2017-09-07 16:58:37 +0100

    Initial commit

---
+++ Dom4Mapper/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MapNumbering
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+      this.progressBar1 = new System.Windows.Forms.ProgressBar();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+      this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(667, 345);
+      this.pictureBox1.TabIndex = 0;
+      this.pictureBox1.TabStop = false;
+      // 
+      // backgroundWorker1
+      // 
+      this.backgroundWorker1.WorkerReportsProgress = true;
+      this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+      this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+      this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+      // 
+      // progressBar1
+      // 
+      this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.progressBar1.Location = new System.Drawing.Point(0, 345);
+      this.progressBar1.Name = "progressBar1";
+      this.progressBar1.Size = new System.Drawing.Size(667, 23);
+      this.progressBar1.TabIndex = 1;
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.DefaultExt = "*.png";
+      this.openFileDialog1.FileName = "openFileDialog1";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(667, 368);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.progressBar1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    private System.Windows.Forms.ProgressBar progressBar1;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+  }
+}
+
---
+++ Dom4Mapper/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapNumbering
+{
+  public partial class Form1 : Form
+  {
+    private Bitmap MyImage;
+    private string MyImageFilename;
+    public Form1()
+    {
+      InitializeComponent();
+      if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+      {
+        MyImageFilename = openFileDialog1.FileName;
+        ShowMyImage(MyImageFilename, pictureBox1.Width, pictureBox1.Height);
+        backgroundWorker1.RunWorkerAsync();
+      }
+    }
+
+    public void ShowMyImage(String fileToDisplay, int xSize, int ySize)
+    {
+      // Sets up an image object to be displayed.
+      if (MyImage != null)
+      {
+        MyImage.Dispose();
+      }
+      MyImage = new Bitmap(fileToDisplay);
+    }
+
+    bool RectIncludeNeighboringPoint(ref Rectangle r, Point p)
+    {
+      if (r.Contains(p))
+      {
+        // Nothing to do
+        return true;
+      }
+
+      bool success = false;
+      // See if the point is neighbouring & grow if so
+      if (r.Contains(p.X - 1, p.Y))
+      {
+        r.Width++;
+        success = true;
+      }
+      if (r.Contains(p.X + 1, p.Y))
+      {
+        r.X--;
+        r.Width++;
+        success = true;
+      }
+      if (r.Contains(p.X, p.Y - 1))
+      {
+        r.Height++;
+        success = true;
+      }
+      if (r.Contains(p.X, p.Y + 1))
+      {
+        r.Y--;
+        r.Height++;
+        success = true;
+      }
+
+      return success;
+    }
+
+    private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+    {
+
+      List<Rectangle> provs = new List<Rectangle>();
+      // parse image for regions
+      for (int y = MyImage.Height-1; y >= 0; y--)
+      {
+        for (int x = 0; x < MyImage.Width; x++)
+        {
+          var col = MyImage.GetPixel(x, y);
+          if (col.R == 255 && col.G == 255 && col.B == 255)
+          {
+            // This is a Province
+            // Filter to check to see if we already have a province right next to this
+            bool dupe = false;
+            for (int i= 0; i< provs.Count; i++)
+            {
+              var prov = provs[i];
+              if (RectIncludeNeighboringPoint(ref prov, new Point(x,y)))
+              {
+                // Dupe found
+                dupe = true;
+                provs[i] = prov;
+                continue;
+              }
+            }
+            // new prov found
+            if (!dupe)
+            {
+              provs.Add(new Rectangle(x, y, 1, 1));
+            }
+          }
+        }
+        (sender as BackgroundWorker).ReportProgress((y * 100) / MyImage.Height);
+      }
+
+      // render the region numbers
+      int num = 1;
+      using (var graphics = Graphics.FromImage(MyImage))
+      {
+        Brush b = new SolidBrush(Color.Black);
+        Font f = new Font("Arial", 140, FontStyle.Bold);
+        foreach (Rectangle r in provs)
+        {
+          var sz = graphics.MeasureString(num.ToString(), f, 10000);
+          Point pt = new Point(r.X - (int)(sz.Width / 2), r.Y - (int)(sz.Height / 2));
+          if (pt.X < 0)
+            pt.X = 0;
+          if (pt.Y < 0)
+            pt.Y = 0;
+          if (pt.X + sz.Width > MyImage.Width)
+            pt.X = MyImage.Width - (int)sz.Width;
+          if (pt.Y + sz.Height > MyImage.Height)
+            pt.Y = MyImage.Height - (int)sz.Height;
+          graphics.DrawString(num.ToString(), f, b, pt);
+          num++;
+        }
+      }
+    }
+
+    private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+    {
+      this.progressBar1.Value = e.ProgressPercentage;
+      this.progressBar1.Refresh();
+    }
+
+    private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+      progressBar1.Visible = false;
+      // Stretches the image to fit the pictureBox.
+      pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+      pictureBox1.ClientSize = new Size(pictureBox1.Width, pictureBox1.Height);
+      pictureBox1.Image = (Image)MyImage;
+
+      string outFile = System.IO.Path.GetDirectoryName(MyImageFilename) + @"\" +
+        System.IO.Path.GetFileNameWithoutExtension(MyImageFilename) +
+        "_numbered" +
+        System.IO.Path.GetExtension(MyImageFilename);
+
+      MyImage.Save(outFile);
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+
+}
